e25d8b31d900704f9a818985daad460c0e1b404d
Abel-A16 <zerubabelendale16@gmail.com>
2022-06-10 18:28:36 +0300

    register-version-1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser")
 app.get("/", function(req, res){
 	res.send("hello abel");
 });
@@ -18,6 +19,12 @@ app.get("/abel/:name", (req, res) => {
 	const name = req.params.name;
 	res.send(name)
 })
+
+app.post("/input", (req, res) => {
+        const name = req.params.name;
+        res.send(name)
+})
+
 app.listen(3000, function(){
         console.log("server started at 3000");
 });
